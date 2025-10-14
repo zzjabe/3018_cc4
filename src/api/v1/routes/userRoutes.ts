a44730396f803dcf2e694a26d84c8ce8eaea25f3
Ronen Begelfor <rbegelfor@rrc.ca>
2025-10-14 18:43:44 -0500

    Added admin routes required from step 2.

--- src/api/v1/routes/userRoutes.ts
+++ src/api/v1/routes/userRoutes.ts
@@ -1,5 +1,8 @@
 import express, { Router } from "express";
 import { getUserProfile, deleteUser } from "../controllers/userController";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
+import { getUserDetails, setCustomClaims } from "../controllers/adminController";
 
 const router: Router = express.Router();
 
@@ -8,5 +11,9 @@ router.get("/profile", getUserProfile);
 
 /** Route to delete a user - requires authentication and admin role */
 router.delete("/:id", deleteUser);
+
+router.post("/users/", authenticate, setCustomClaims);
+
+router.get("/users/:id", getUserDetails);
 
 export default router;
